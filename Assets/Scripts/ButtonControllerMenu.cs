52ee93060baf4576f2f75d8b7c6ec917af675a9e
Ahmet Ceyran <ahmetceyran00@gmail.com>
2022-10-02 18:41:28 +0300

    adding menu buton controller

---
+++ Assets/Scripts/ButtonControllerMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonControllerMenu : MonoBehaviour
+{
+    [SerializeField] private Button btnSinifSecim;
+    [SerializeField] private Button btnLeaderBoard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnSinifSecim.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("SinifSecim");
+    }
+
+}
