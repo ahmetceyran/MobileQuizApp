46c1d68744819bd199a4482280ee12561e268536
Ahmet Ceyran <ahmetceyran00@gmail.com>
2022-10-03 17:00:29 +0300

    Adding CardSO script

---
+++ Assets/Scripts/QuizEso.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "CardSO",fileName = "Card")]
+public class QuizEso : ScriptableObject
+{
+    public string newQuestion;
+    public string newA;
+    public string newB;
+    public string newC;
+    public string newD;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
