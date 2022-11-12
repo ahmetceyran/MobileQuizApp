ed6616ced201d9bcfde0f4fa9c863e8d43cf5325
Ahmet Ceyran <ahmetceyran00@gmail.com>
2022-11-12 19:07:53 +0300

    Update ButtonControllerMenu.cs

--- Assets/Scripts/ButtonControllerS/ButtonControllerMenu.cs
+++ Assets/Scripts/ButtonControllerS/ButtonControllerMenu.cs
@@ -9,12 +9,16 @@ public class ButtonControllerMenu : MonoBehaviour
     [SerializeField] private Button btnSinifSecim;
     [SerializeField] private Button btnLeaderBoard;
     [SerializeField] private Button btnStore;
+    [SerializeField] private GameObject coinDisplay;
+    private int coinAmount;
     // Start is called before the first frame update
     void Start()
     {
         btnSinifSecim.onClick.AddListener(TaskOnClick);
         btnLeaderBoard.onClick.AddListener(TaskOnClick1);
         btnStore.onClick.AddListener(TaskOnClick2);
+        coinAmount = PlayerPrefs.GetInt("CoinAmount");
+        coinDisplay.GetComponent<Text>().text = "Coin : " + coinAmount;
     }
 
     void TaskOnClick()
