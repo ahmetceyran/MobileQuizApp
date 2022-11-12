2f6d2fdaddad07a39d90d7f246a6c2b5fa98096e
Ahmet Ceyran <ahmetceyran00@gmail.com>
2022-11-12 19:10:14 +0300

    Update ButtonControllerStore.cs

--- Assets/Scripts/ButtonControllerS/ButtonControllerStore.cs
+++ Assets/Scripts/ButtonControllerS/ButtonControllerStore.cs
@@ -7,11 +7,14 @@ using UnityEngine.SceneManagement;
 public class ButtonControllerStore : MonoBehaviour
 {
     [SerializeField] private Button btnBack;
-
+    [SerializeField] private GameObject coinDisplay;
+    private int coinAmount;
     // Start is called before the first frame update
     void Start()
     {
         btnBack.onClick.AddListener(TaskOnClick);
+        coinAmount = PlayerPrefs.GetInt("CoinAmount");
+        coinDisplay.GetComponent<Text>().text = "Coin Miktari : " + coinAmount;
     }
 
     void TaskOnClick()
